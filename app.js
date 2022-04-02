abf8ee1be8771133a5826d15e567256319e7af02
Sekai422 <jinsato0422@gmail.com>
2022-04-02 13:15:30 -0600

    Backend stuff (still working on)

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
 const express = require("express"); //setup a basic express server
 const app = express(); //initialize a variable ,app is an express function with built in methods like app.use() app.get
+const path = require('path');
 const port = process.env.PORT||3000; //get an available port else take 3000
-
 const recipe = require("./routes/recipe"); //all similar tasks routes should be imported
 const connectDB = require("./db/connect"); //connect to db
+require('dotenv').config()  
 
-require('dotenv').config()  //
+//Models
+const Recipe = require('./models/recipe');
+
+const mongoose = require("mongoose");                               // get mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/what-the-fridge');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+//Set ejs and path
+app.set('view engine', 'ejs'); //get ejs 
+app.set('views', path.join(__dirname, 'views')); //set path
 
 app.use(express.json()); //parses incoming data to req.body
-
-
 app.use("/api/v1", recipe); //REPLACE THE ENDPOINT WITH SPOONACULAR'S ROUTE
 
+//Route path as set to home
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Show all Recipes in local mongodb
+app.get('/recipes', async (req, res) => {
+    const recipes = await Recipe.find({});
+    res.render('recipes/index', {recipes});
+});
+
+// Create Recipe in local mongodb
+app.get('/makeRecipe', async (req, res) => {
+    const recipe = new Recipe({name: "Taco"});
+    await recipe.save();
+    res.send(recipe);
+});
+
+// Show individual recipe
+app.get('/recipes/:id', async(req, res) => {
+    res.render('recipes/show');
+});
+
+app.listen(3000, () => {
+    console.log("Serving on port 3000");
+})
 
 
 
-
-const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI); //connect to database first before starting server,uri is in .env file and await the connection
+// const start = async () => {
+//   try {
+//     await connectDB(process.env.MONGO_URI); //connect to database first before starting server,uri is in .env file and await the connection
    
-    app.listen(port, console.log(`server is listenin on port ${port}...`));
-  } catch (err) {
-    console.log(err);
-  }
-};
-start()
+//     app.listen(port, console.log(`server is listenin on port ${port}...`));
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+// start()
--- models/recipe.js
+++ models/recipe.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");                               // get mongoose
+const Schema = mongoose.Schema;                                     // for less coding
 
 //API is not considered*** it's just a dummy model........
-const RecipeSchema = new mongoose.Schema({
+const RecipeSchema = new Schema({
 
-  id: {
-    type: String,
-    required:true   
-  },
+//   id: {
+//     type: String,
+//     required:true   
+//   },
   name: {
     type: String,
     required: [true, "must provide a name of recipe"],
